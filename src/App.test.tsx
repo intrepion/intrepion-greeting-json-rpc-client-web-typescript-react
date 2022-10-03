b18fef8fbbbd0cef913354fcbb8ed59aa51ac0e6
Oliver Forral <intrepion@gmail.com>
2022-10-03 16:35:05 -0600

    green

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders hello world element", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Hello, World!/i);
+  expect(element).toBeInTheDocument();
 });
