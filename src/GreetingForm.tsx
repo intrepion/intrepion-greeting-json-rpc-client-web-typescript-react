4326386c1bca11fccd78552d4497eabdc1fcf976
Oliver Forral <intrepion@gmail.com>
2022-12-17 19:40:37 -0700

    npx prettier --write .

--- src/GreetingForm.tsx
+++ src/GreetingForm.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 
-const SERVER_URL =
-  process.env.REACT_APP_SERVER_URL ?? "http://localhost:3000";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL ?? "http://localhost:3000";
 
 interface GreetingJsonRpcParams {
   name: string;
